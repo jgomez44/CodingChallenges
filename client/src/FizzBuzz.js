655a6bf9639024c1f83e9d0a483528a7ddc87a44
Jessica <jgomeznd@gmail.com>
2019-01-13 20:53:33 -0800

    fizzbuzz printing to console

--- client/src/FizzBuzz.js
+++ client/src/FizzBuzz.js
@@ -1,20 +1,20 @@
 import React from "react";
 
 class FizzBuzz extends React.Component {
-  handleFizzBuzz(num) {
-    for (let i = 0; (i = num); i++) {
-      if (num % 2 === 0) {
+  handleFizzBuzz = num => {
+    for (let i = 0; i < num + 1; i++) {
+      if (i % 3 === 0) {
         console.log("fizz");
       }
-      if (num % 5 === 0) {
+      if (i % 5 === 0) {
         console.log("buzz");
       } else {
-        console.log(num);
+        console.log(i);
       }
     }
-  }
+  };
   render() {
-    return <>{this.handleFizzBuzz(100)}</>;
+    return <>{this.handleFizzBuzz(10)}</>;
   }
 }
 
