c59b30fee252944748d3e772bf926e330e602cc2
Jessica <jgomeznd@gmail.com>
2019-01-15 16:29:37 -0800

    reverse string loop

--- client/src/ReverseString.js
+++ client/src/ReverseString.js
@@ -1,14 +1,27 @@
 import React from "react";
 
 class ReverseString extends React.Component {
-  reverseString(a) {
-    return a
+  reverseString(str) {
+    return str
       .split("")
       .reverse()
       .join("");
   }
+
+  reverseStringLoop(str) {
+    let reverseStr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+      reverseStr += str[i];
+    }
+    return reverseStr;
+  }
   render() {
-    return <>{console.log(this.reverseString("apple"))}</>;
+    return (
+      <>
+        {console.log(this.reverseString("apple"))}
+        {console.log(this.reverseStringLoop("orange"))}
+      </>
+    );
   }
 }
 export default ReverseString;
